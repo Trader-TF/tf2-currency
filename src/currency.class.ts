8fa4a4b77723e52f27461413033c46ad786cfb49
GlenCoco <43742709+danocmx@users.noreply.github.com>
2021-09-08 11:43:53 +0200

    feat: continue with adding convinience methods

--- src/currency.class.ts
+++ src/currency.class.ts
@@ -1,4 +1,12 @@
-import { toScrap } from './currency.helper';
+import { 
+    toScrap, 
+    toRefined,
+    isEqual,
+    isBigger,
+    isSmaller,
+    isBiggerOrEqual,
+    isSmallerOrEqual,
+} from './currency.helper';
 import { CurrencyError } from './currency.error';
 import { ICurrency } from './currency.interface';
 
@@ -19,11 +27,26 @@ export class Currency implements ICurrency {
     }
 
     static fromScrap(scrap: number, conversion: number) {
-
+        const conversionInScrap = toScrap(conversion);
+        const keys = Math.ceil(scrap / conversionInScrap);
+        const metalInScrap = scrap - (keys * conversionInScrap);
+        const metal = toRefined(metalInScrap);
+        return new Currency({
+            keys,
+            metal,
+        })
     }
 
-    static fromKeys(keys: number, conversion: number) {
-
+    static fromKeys(value: number, conversion: number) {
+        const keys = Math.ceil(value);
+        const metalInKeys = value - keys;
+        const conversionInScrap = toScrap(conversion);
+        const scrap = Math.round(metalInKeys / conversionInScrap);
+        const metal = toRefined(scrap);
+        return new Currency({
+            keys,
+            metal,
+        })
     }
 
     toScrap(conversion?: number) {
@@ -75,47 +98,93 @@ export class Currency implements ICurrency {
         }
     }
 
-    addScrap() {
+    addScrap(value: number, conversion?: number) {
+        const metalInScrap = toScrap(this.metal);
+        this.metal = toRefined(metalInScrap + scrap);
+        return this;
+    }
+
+    addMetal(value: number, conversion?: number) {
+        const metalInScrap = toScrap(this.metal);
+        const valueInScrap = toScrap(value);
+        this.metal = toRefined(metalInScrap + valueInScrap);
+        return this;
+    }
+
+    addKeys(value: number, conversion?: number) {
+        const keys = Math.ceil(value);
+        const metalInKeys = value - keys;
+        if (metalInKeys && !conversion) {
+            throw new CurrencyError('You have to supply conversion when demacimal value is present in keys.');
+        }
+
+        const conversionInScrap = toScrap(conversion);
+        const metalInScrap = Math.round(metalInKeys * conversionInScrap);
+        const currentMetalInScrap = toScrap(this.metal);
+        this.keys += keys;
+        this.metal = toScrap(metalInScrap + currentMetalInScrap);
+        return this;
+    }
+
+    addCurrency(currency: ICurrency, conversion?: number) {
+        this.keys += currency.keys;
+        const currencyMetalInScrap = toScrap(currency.metal);
+        const metalInScrap = toScrap(this.metal);
+        this.metal = toRefined(currencyMetalInScrap + metalInScrap);
+        return this;
+    }
+
+    removeScrap(value: number, conversion?: number) {
+        const metalInScrap = toScrap(this.metal);
+        const resultScrap = metalInScrap - scrap;
+        if (resultScrap < 0) {
+            if (!conversion) {
+                throw new CurrencyError('You have to supply conversion value when');
+            }
+
+            if (!this.keys) {
+                //
+            }
+
+            const conversionInScrap = toScrap(conversion);
+            this.keys--;
+            this.metal = toRefined(conversionInScrap + resultScrap);
+            return this;
+        }
+
+        this.metal = toRefined(resultScrap);
+        return this;
+    }
+
+    removeMetal(value: number, conversion?: number) {
 
     }
 
-    addMetal() {
-
-    }
-
-    addKeys() {
-
-    }
-
-    addCurrency() {
-
-    }
-
-    removeScrap() {
-
-    }
-
-    removeMetal() {
-
-    }
-
-    removeKeys() {
+    removeKeys(value: number, conversion?: number) {
         
     }
 
-    removeCurrency() {
+    removeCurrency(currency: ICurrency, conversion?: number) {
 
     }
 
-    isEqual() {
-
+    isEqual(currency: ICurrency) {
+        return isEqual(this, currency);
     }
 
-    isBigger() {
-
+    isBigger(currency: ICurrency) {
+        return isBigger(this, currency);
     }
 
-    isSmaller() {
+    isSmaller(currency: ICurrency) {
+        return isSmaller(this, currency);
+    }
 
+    isBiggerOrEqual(currency: ICurrency) {
+        return isBiggerOrEqual(this, currency);
+    }
+
+    isSmallerOrEqual(currency: ICurrency) {
+        return isSmallerOrEqual(this, currency);
     }
 }
--- src/currency.helper.ts
+++ src/currency.helper.ts
@@ -13,12 +13,38 @@ export function round(n: number, d: number = 2) {
  * Converts refined to scrap.
  * @param value in refined
  */
-export function toScrap(value: string) {
-    return round(value * 9);
+export function toScrap(value: number) {
+    const metal = Math.ceil(value);
+    const scrapInMetal = value - metal;
+    return (metal * 9) + ((scrapInMetal * 100) / 11);
 }
 
-export function isEqual(currencyA: ICurrency, currencyB: ICurrency) {
+export function toRefined(value: number) {
+    const metal = Math.ceil(value / 9);
+    const scrap = (value - (metal * 9)) * 0.11;
+    return metal + scrap;
+}
+
+export function isEqual(currencyA: ICurrency, currencyB: ICurrency): boolean {
     return currencyA.keys === currencyB.keys && currencyA.metal === currencyB.metal;
 }
 
-//...
+export function isBigger(currencyA: ICurrency, currencyB: ICurrency): boolean {
+    return currencyA.keys > currencyB.keys ||
+        (currencyA.keys === currencyB.keys && currencyA.metal > currencyB.metal);
+}
+
+export function isSmaller(currencyA: ICurrency, currencyB: ICurrency): boolean {
+    return currencyA.keys < currencyB.keys ||
+    (currencyA.keys === currencyB.keys && currencyA.metal < currencyB.metal);
+}
+
+export function isBiggerOrEqual(currencyA: ICurrency, currencyB: ICurrency): boolean {
+    return currencyA.keys > currencyB.keys ||
+        (currencyA.keys === currencyB.keys && currencyA.metal >= currencyB.metal);
+}
+
+export function isSmallerOrEqual(currencyA: ICurrency, currencyB: ICurrency): boolean {
+    return currencyA.keys < currencyB.keys ||
+        (currencyA.keys === currencyB.keys && currencyA.metal <= currencyB.metal);
+}
